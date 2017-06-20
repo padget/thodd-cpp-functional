e6fa92ac31519b0d350647e1621b0438996c96fd
padget <padget.pro@gmail.com>
2017-06-20 16:03:39 +0200

    split into files

---
+++ include/functor.hpp
@@ -0,0 +1,83 @@
+#ifndef __THODD_FUNCTIONAL_FUNCTOR_HPP__
+#  define __THODD_FUNCTIONAL_FUNCTOR_HPP__
+
+#  include <type_traits>
+
+namespace 
+thodd
+{
+    template<
+        typename base_t>
+    struct functor ;
+
+
+    constexpr auto
+    as_functor(
+        auto&& __base)
+    {
+        return 
+        functor<decltype(__base)>
+        { static_cast<decltype(__base)&&>(__base) } ;
+    }
+
+
+    template<
+        typename base_t>
+    struct functor
+    {
+        std::decay_t<base_t> base;
+
+        constexpr decltype(auto)
+        operator () (
+            auto&&... __args) const
+        {
+            return 
+            base(static_cast<decltype(__args)&&>(__args)...) ;
+        }
+
+
+        template<
+            typename obase_t>
+        struct assignement
+        {
+            functor<base_t> this_;
+            functor<obase_t> other;
+
+            constexpr auto
+            operator () (
+                auto&&... __args) const
+            -> decltype(auto)
+            {
+                return 
+                this_(static_cast<decltype(__args)&&>(__args)...) = 
+                    other(static_cast<decltype(__args)>(__args)...) ;
+            }
+        } ;
+
+
+        template<
+            typename obase_t>
+        constexpr auto
+        operator = (
+            functor<obase_t> const& __other) const
+        {
+            return 
+            as_functor(
+                assignement<obase_t>{*this, __other}) ;
+        }
+
+        template<
+            typename obase_t>
+        constexpr auto
+        operator = (
+            functor<obase_t> && __other) const
+        {
+            return 
+            as_functor(
+                assignement<obase_t>{*this, __other}) ;
+        }
+    };
+}
+
+
+#endif
---
+++ include/statements.hpp
@@ -0,0 +1,75 @@
+#ifndef __THODD_FUNCTIONAL_STATEMENTS_HPP__
+#  define __THODD_FUNCTIONAL_STATEMENTS_HPP__
+
+#  include <thodd/functional/functor.hpp>
+
+namespace
+thodd
+{
+    template<
+        typename left_t,
+        typename right_t>
+    struct statements
+    {
+        std::decay_t<left_t> left;
+        std::decay_t<right_t> right;
+
+        template<
+            typename ... args_t>
+        constexpr auto
+        operator () (
+            args_t&&... _args) const
+        -> decltype(auto)
+        {
+            left(static_cast<decltype(_args)&&>(_args)...) ;
+            
+            return right(static_cast<decltype(_args)&&>(_args)...) ;
+        }
+    };
+
+    template<
+        typename base_t,
+        typename base2_t>
+    constexpr auto
+    operator , (
+        functor<base_t> const& _functor,
+        functor<base2_t> const& _functor2)
+    {
+        return as_functor(statements<base_t, base2_t>{_functor.base, _functor2.base});
+    }
+
+    template<
+        typename base_t,
+        typename base2_t>
+    constexpr auto
+    operator , (
+        functor<base_t> const& _functor,
+        functor<base2_t> && _functor2)
+    {
+        return as_functor(statements<base_t, base2_t>{_functor.base, _functor2.base});
+    }
+
+    template<
+        typename base_t,
+        typename base2_t>
+    constexpr auto
+    operator , (
+        functor<base_t> && _functor,
+        functor<base2_t> const& _functor2)
+    {
+        return as_functor(statements<base_t, base2_t>{_functor.base, _functor2.base});
+    }
+
+    template<
+        typename base_t,
+        typename base2_t>
+    constexpr auto
+    operator , (
+        functor<base_t> && _functor,
+        functor<base2_t> && _functor2)
+    {
+        return as_functor(statements<base_t, base2_t>{_functor.base, _functor2.base});
+    }
+}
+
+#endif
